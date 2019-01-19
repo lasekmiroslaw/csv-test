6320733fabaeb7881c18d49d0e2d89cf783b8cfe
lasekmiroslaw <lasekmiroslaw@gmail.com>
2019-01-19 13:36:21 +0100

    Add home view with navbar

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+}
